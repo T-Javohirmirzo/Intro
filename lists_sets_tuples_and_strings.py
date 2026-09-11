c7c4e5a8b95cc1ebd3cd7e86e16d91f581ca787e
T-Javohirmirzo <javtoshov26@uwcchina.org>
2026-09-11 14:29:37 +0800

    Create lists_sets_tuples_and_strings.py

---
+++ lists_sets_tuples_and_strings.py
@@ -0,0 +1,74 @@
+# fruits = ["apple", "banana", "cherry", "strawberry", "elderberry"]
+
+
+# print(fruits[::-1])
+
+# for x in fruits:
+#     print(x)
+
+# print(len(fruits))
+
+# print("banana" in fruits)
+
+# fruits.append("orange")
+
+# for fruit in fruits:
+#     print(fruit)
+
+# print(len(fruits))
+
+# fruits.remove("apple")
+# fruits.pop(0)  
+
+# print(len(fruits))
+
+# for fruit in fruits:
+#     print(fruit)
+# print(fruits)
+# fruits.insert(0, "kiwi")
+# print(fruits)
+
+# fruits.insert(0, "elderberry")
+
+# print(fruits.index("elderberry"))
+
+# print(fruits.count("elderberry"))
+
+# fruits = {"apple", "banana", "cherry", "strawberry", "elderberry"}
+
+# print(fruits)
+
+# fruits = ("apple", "banana", "cherry", "strawberry", "elderberry")
+# fruits = fruits + ("kiwi", "mango")
+# print(fruits[0])
+
+# name = input("Enter your name: ")
+
+# result = len(name)
+
+# print(result)
+
+# result = name.rfind("a")
+
+# print(result)
+
+
+# username = input("Enter a username: ")
+
+# username.isalpha()
+# if len(username)>12:
+#    print("Your username cannot be more than 12 characters")
+# elif not username.find(" ") == -1:
+#    print("Your username cannot contain spaces")
+# elif not username.isalpha():
+#    print("Your username has digits")
+# else:
+#    print(f"Welcome {username}")   
+
+credit_number = "1234-5678-9012-3456"
+print(len(credit_number))
+print(credit_number[18])
+
+# print(credit_number[0:4])
+
+print(credit_number[5:9])
